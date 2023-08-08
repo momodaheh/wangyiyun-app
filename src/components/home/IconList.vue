76d7af638b4a1cafc3511644fa4ab48692d3ac8a
momoda <WZY450416@outlook.com>
2023-08-08 19:26:37 +0800

    :feat icon导航条样式

---
+++ src/components/home/IconList.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="iconList">
+        <div class="iconItem">
+            <svg class="icon" aria-hidden="true">
+                 <use xlink:href="#icon-tubiaozhizuomoban"></use>
+            </svg>
+            <span>每日推荐</span>
+        </div>
+        <div class="iconItem">
+            <svg class="icon" aria-hidden="true">
+                 <use xlink:href="#icon-diantai"></use>
+            </svg>
+            <span>私人FM</span>
+        </div>
+        <div class="iconItem">
+            <svg class="icon" aria-hidden="true">
+                 <use xlink:href="#icon-gedan"></use>
+            </svg>
+            <span>歌单</span>
+        </div>
+        <div class="iconItem">
+            <svg class="icon" aria-hidden="true">
+                 <use xlink:href="#icon-paihangbang"></use>
+            </svg>
+            <span>排行榜</span>
+        </div>
+
+    </div>
+</template>
+
+<style lang="less" scoped>
+.iconList{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    width: 100%;
+    height: 2rem;
+    .iconItem{
+        width: 25%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        .icon{
+            height: 1rem;
+            width: 1rem;
+        }
+    }
+}
+
+
+</style>
